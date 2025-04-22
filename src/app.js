cba82ccc9a90255c0a1b582a2805a9feb60fd4ba
shuvabiswas12 <shuvabiswas12@gmail.com>
2025-04-22 20:45:05 +0600

    ApiError and ApiResponse file added and asyncHandler added.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 export const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+
+// passing data through nesting object through url.
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+// for accessing secure cookie form browser.
+app.use(cookieParser());
