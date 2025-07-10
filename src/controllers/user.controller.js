44c153e8be496c7023cf1afa4b03f17fe27527d7
shuvabiswas12 <shuvabiswas12@gmail.com>
2025-07-10 21:02:09 +0600

    bug fixed on user controller.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required.");
     }
 
-    const existingUser = User.findOne({ $or: [{ username }, { email }] });
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
 
     if (existingUser) {
         throw new ApiError(409, "User with email or username already exists.");
