ded9c6b6202d997a945bf7a2cc0b5172e62d9b05
shuvabiswas12 <shuvabiswas12@gmail.com>
2025-07-11 18:25:26 +0600

    add refresh token functionality and secure logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,10 @@ import {
     loginUser,
     logoutUser,
     registerUser,
+    refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -26,6 +28,7 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
